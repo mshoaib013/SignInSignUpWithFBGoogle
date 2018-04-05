6a45a076b5e9b54abe3554ed1b6963e7ac6eb5d4
darkshadow <shoaib.mehedi@gmail.com>
2018-04-06 01:35:52 +0600

    1st upload

---
+++ app/src/main/java/com/google/firebase/quickstart/auth/MainActivity.java
@@ -0,0 +1,57 @@
+package com.google.firebase.quickstart.auth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "log";
+
+    Button login;
+    Button getStarted;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login =(Button) findViewById(R.id.login);
+        getStarted =(Button) findViewById(R.id.get_started);
+//        Intent login = new Intent(this, login.class);
+//        Intent signin = new Intent(this, signUp.class);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(MainActivity.this, login.class);
+                startActivity(login);
+            }
+        });
+
+        getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signup = new Intent(MainActivity.this, signUp.class);
+                startActivity(signup);
+            }
+        });
+
+
+
+//        facebook.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "onClick: ");
+//                startActivity(facebookIntent);
+//            }
+//        });
+//        google.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(googleIntent);
+//            }
+//        });
+    }
+}
